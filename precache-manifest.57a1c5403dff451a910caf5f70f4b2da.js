16417d885f1bf378c7dff97d940a159223f5899c
mavin19 <koung.mavin@gmail.com>
2020-05-14 20:48:31 +0700

    Updates

---
+++ precache-manifest.57a1c5403dff451a910caf5f70f4b2da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233c8ee8efad86c7e592c15edd06a4a3",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "d0dec6939da73ebde1b6",
+    "url": "/deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "09b0ac98dfe2d6cf96fc",
+    "url": "/deploy-test/static/js/2.5761c3f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-test/static/js/2.5761c3f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0dec6939da73ebde1b6",
+    "url": "/deploy-test/static/js/main.03572b18.chunk.js"
+  },
+  {
+    "revision": "0eaf70e23a6565c37934",
+    "url": "/deploy-test/static/js/runtime-main.ea0620f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
